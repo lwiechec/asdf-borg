edecdcda231b9f98a921589a043e615514c1879f
Łukasz Wiecheć <lwiechec@o2.pl>
2022-08-09 12:52:17 +0200

    excluding all 'dev' releases

--- lib/utils.bash
+++ lib/utils.bash
@@ -11,7 +11,7 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if babashka is not hosted on GitHub releases.
+# NOTE: You might want to remove this if borg is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -24,7 +24,7 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/dev//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
